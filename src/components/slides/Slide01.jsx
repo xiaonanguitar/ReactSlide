c9efcd4133f807a7fef55793a728f161783fe614
xiaonanguitar <zhangzhinan1987@126.com>
2017-08-20 18:21:58 +0800

    text debugger from chrome

--- src/components/slides/Slide01.jsx
+++ src/components/slides/Slide01.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import { Table,Input,Modal,Spin,Button,Popconfirm  } from 'antd';
+import { Table, Input, Modal, Spin, Button, Popconfirm } from 'antd';
 import _ from 'lodash';
 import ReactSVG from 'react-svg';
 import '../styles/slide01.css';
 
 class Slide01 extends Component {
 
-    constructor(props,context) {
+    constructor(props, context) {
         super(props)
     }
 
     render() {
         return (
             <div className="mainpage">
-                <div style={{padding: "50px 0"}}>
+                <div>
                     {/*<ReactSVG path="../../images/logo.svg" className="logo"/>*/}
-                    <img className="logo" src="../../images/reactjs.png"/>
-                    <a 
-                        href="https://facebook.github.io/react/" 
+                    <img className="logo" src="../../images/reactjs.png" />
+                    <a
+                        href="https://facebook.github.io/react/"
                         className="mainpage_label" >
                         ReactJS Foundamentals
                     </a>
